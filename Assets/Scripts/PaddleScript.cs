44be8a7cce67a325b4ab9861cd27dc498bd314bb
neos-y.kobayashi <y.kobayashi@neoscorp.jp>
2019-12-16 14:53:25 +0900

    初回コミット

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, 0f);
+        // Debug.Log(transform.position.x);
+    }
+}
